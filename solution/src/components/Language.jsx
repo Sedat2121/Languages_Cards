2f3f8ed5eca1ff6da8421e5d0d34c75515c32010
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-28 21:15:52 +0000

    language.jsx is all set and linked to the app.js

--- solution/src/components/Language.jsx
+++ solution/src/components/Language.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
+const Language = ({ name, img, options }) => {
+     const [showImage, setShowImage] = useState(true);
+     return (
+          <Container 
+               style={{ background: "peachpuff" }}
+               className="p-4 rounded-4 h-100 lang-card"
+               onClick={() => setShowImage( !showImage )} 
+               type="button"
+          >
 
-const Language = () => {
-  return (
-    <div>Language</div>
-  )
-}
+               { showImage && (
+                    <Container>
+                         <Image className="lang-logo" src={img} width="70%" ></Image>
+                         <h3 className="display-6">{ name }</h3>
+                    </Container>
+               )}
 
-export default Language
+               { !showImage && (
+                    <ol>
+                         {options.map((item) => {
+                              return <li className="h5 text-start">{item}</li>;
+                         })}
+                    </ol>
+               )}
+          </Container>
+     );
+};
+
+export default Language;
